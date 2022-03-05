b066fa31cf8f2bee2d72e5d1188b5ce522fbc53d
IS709090 <is709090@iteso.mx>
2022-03-05 10:20:46 -0600

    Create APICalls.dart
    Se crea el archivo APICalls.dart en donde mencionamos todos los Requests HTTP que se realizaron a las APIs.

---
+++ lib/APICalls.dart
@@ -0,0 +1,15 @@
+//              repositorio de APICalls que se hicieron
+//
+//  https://flagcdn.com/32x24/ad.png
+//    Se hace el request de la bandera de Andorra con las dimensiones 32x24
+//  https://flagcdn.com/32x24/mx.png
+//    Se hace el request de la bandera de Mexico con las dimensiones 32x24
+//  https://flagcdn.com/32x24/pe.png
+//    Se hace el request de la bandera de Peru con las dimensiones 32x24
+//  https://flagcdn.com/32x24/ca.png
+//    Se hace el request de la bandera de Canada con las dimensiones 32x24
+//  https://flagcdn.com/32x24/ar.png
+//    Se hace el request de la bandera de Argentina con las dimensiones 32x24
+//
+//  https://picsum.photos/200/300
+//    Se hace el request de una imagen random con una resolucion de 200x300
